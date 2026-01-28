41303133d65aeb5b9126b97cba397f5c7a75a6e6
Youssef Masry <youssefmasry888@gmail.com>
2026-01-28 02:34:00 +0200

    Fix router duplication for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  );
+  return <AppRoutes />;
 }
