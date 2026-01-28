1dc7ca6d0dc2e1454a45dc14c9d28c54594234e8
Youssef Masry <youssefmasry888@gmail.com>
2026-01-28 02:15:22 +0200

    Deploy via docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  bass: "/task_mintor/",
-  plugins: [
-     tailwindcss(),
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  base: "/task_mintor/",
+  plugins: [tailwindcss(),
+     react()],
+  build: {
+    outDir: "docs",
+  },
+});
